9e2cdf6baba6be6a0c6419971b071e49d85c6093
Saud Ahmed <saudahmed96x@gmail.com>
2024-01-20 12:54:23 -0500

    i forgot to push

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to my Express App </h1>`);
+  res.send(`<h1>Welcome to my App </h1>`);
 });
 
 app.listen(PORT, () => {
-  res.send(`Listening on PORT${PORT}`);
+  console.log(`app running on port ${PORT}`);
 });
